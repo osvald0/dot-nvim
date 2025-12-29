2186947440351aa802a7c19e5d9279ea7f76b654
Osvaldo Colina <osvaldo.colina@gmail.com>
2025-12-28 21:32:20 -0300

    update nvim-tree to close node when press h

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -6,6 +6,20 @@ return {
 		"nvim-tree/nvim-web-devicons",
 	},
 	config = function()
-		require("nvim-tree").setup({})
+		require("nvim-tree").setup({
+			on_attach = function(bufnr)
+				local api = require("nvim-tree.api")
+
+				api.config.mappings.default_on_attach(bufnr)
+
+				vim.keymap.set("n", "h", api.node.navigate.parent_close, {
+					desc = "nvim-tree: Close Directory",
+					buffer = bufnr,
+					noremap = true,
+					silent = true,
+					nowait = true,
+				})
+			end,
+		})
 	end,
 }
